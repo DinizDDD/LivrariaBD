1ec922613a1b279f4de789d51f0b4dde58a21cfe
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-05-08 15:49:40 -0300

    fix some errors

--- livraria.sql
+++ livraria.sql
@@ -9,16 +9,17 @@ create table autor(
     id_autor int PRIMARY KEY,
     nome varchar(255),
     nacionalidade varchar(255)
-)
+);
 
 -- criei a tabela livros
 create table livro(
     id_livro int PRIMARY KEY,
     titulo varchar(255),
     ano_publicacao int,
+    id_autor int,
     FOREIGN KEY (id_autor) REFERENCES autores(id_autor),
     preco decimal
-)
+);
 
 -- criei a tabela clientes
 create table cliente(
@@ -26,13 +27,15 @@ create table cliente(
     nome varchar(255),
     email varchar(255),
     cidade varchar(255),
-)
+);
 
 -- creiei a tabela vendas
 create table venda(
     id_venda int PRIMARY KEY,
+    id_cliente int,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    id_livro int,
     FOREIGN KEY (id_livro) REFERENCES livro(id_livro),
     data_venda date,
     quantidade int
-)
+);
