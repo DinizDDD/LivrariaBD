6e81e56b9e2c697cdb8099397aa88a5f254b9de7
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-05-09 15:23:19 -0300

    update the values

--- livraria.sql
+++ livraria.sql
@@ -6,36 +6,36 @@ use Livraria;
 
 -- criei a tabela autores
 create table autor(
-    id_autor int PRIMARY KEY,
-    nome varchar(255),
-    nacionalidade varchar(255)
+    id_autor int PRIMARY KEY not null,
+    nome varchar(255) not null,
+    nacionalidade varchar(255) not null
 );
 
 -- criei a tabela livros
 create table livro(
-    id_livro int PRIMARY KEY,
-    titulo varchar(255),
-    ano_publicacao int,
-    id_autor int,
-    FOREIGN KEY (id_autor) REFERENCES autores(id_autor),
-    preco decimal
+    id_livro int PRIMARY KEY not null,
+    titulo varchar(255) not null,
+    ano_publicacao int not null,
+    id_autor int not null,
+    FOREIGN KEY (id_autor) REFERENCES autor(id_autor),
+    preco decimal not null
 );
 
 -- criei a tabela clientes
 create table cliente(
-    id_cliente int PRIMARY KEY,
-    nome varchar(255),
-    email varchar(255),
-    cidade varchar(255),
+    id_cliente int PRIMARY KEY not null,
+    nome varchar(255) not null,
+    email varchar(255) not null,
+    cidade varchar(255) not null
 );
 
 -- creiei a tabela vendas
 create table venda(
-    id_venda int PRIMARY KEY,
-    id_cliente int,
+    id_venda int PRIMARY KEY not null,
+    id_cliente int not null,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-    id_livro int,
+    id_livro int not null,
     FOREIGN KEY (id_livro) REFERENCES livro(id_livro),
-    data_venda date,
-    quantidade int
+    data_venda date not null,
+    quantidade int not null
 );
